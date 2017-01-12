4e8598388b8957764af43ea14add4ac0f424c19e
qing <qing@qings-iMac.local>
2017-01-12 23:40:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/qing/mysqlitetest/TestSQLiteActivity.java
@@ -0,0 +1,13 @@
+package com.example.qing.mysqlitetest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestSQLiteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_sqlite);
+    }
+}
